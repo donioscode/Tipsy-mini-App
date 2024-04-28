cff00b2836166ebeebe7eed7a8b2d0380ca9cb48
donioscode <doniyornormuxammedov@gmail.com>
2024-04-28 14:30:29 +0500

    first get present value

---
+++ Tipsy-mini-App/Controller/CalculatorViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Tipsy-mini-App
+//
+//  Created by doniyor normuxammedov on 28/04/24.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTF: UITextField!
+    
+    @IBOutlet weak var zeroPctBtn: UIButton!
+    @IBOutlet weak var tenPctBtn: UIButton!
+    @IBOutlet weak var twentyPctBtn: UIButton!
+    
+    @IBOutlet weak var splitNumberLB: UILabel!
+    
+    
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValuesChanged(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+}
+
--- Tipsy-mini-App/Controller/ResultsViewController.swift
+++ Tipsy-mini-App/Controller/ResultsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ResultsViewController.swift
 //  Tipsy-mini-App
 //
 //  Created by doniyor normuxammedov on 28/04/24.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ResultsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
-
 }
-
