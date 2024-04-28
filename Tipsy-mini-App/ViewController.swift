b57a94d9019e84be377ec16ab97649549541ef3f
donioscode <doniyornormuxammedov@gmail.com>
2024-04-28 11:49:55 +0500

    Initial Commit

---
+++ Tipsy-mini-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy-mini-App
+//
+//  Created by doniyor normuxammedov on 28/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
